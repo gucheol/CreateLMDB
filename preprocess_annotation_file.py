4bebceadc498732625f983e7661b132cda9554f0
gucheol <9fe.kwon@apptest.ai>
2021-10-28 18:21:46 +0900

    정리 및 데이터 비율에 따라 lmdb 생성 기능 추가

--- preprocess_annotation_file.py
+++ preprocess_annotation_file.py
@@ -2,15 +2,181 @@ import cv2
 import os
 import glob
 import numpy as np
+import logging
+logging.getLogger().setLevel(logging.INFO)
 
-def align_points(box):
-    # Make it clockwise align
-    # box = np.array([(int(float(x)), int(float(y))) for x, y in zip(items[1::2], items[2::2])])
-    centroid = np.sum(box, axis=0) / 4
-    theta = np.arctan2(box[:, 1] - centroid[1], box[:, 0] - centroid[0]) * 180 / np.pi
-    indices = np.argsort(theta)
-    aligned_box = box[indices]
-    return aligned_box
+
+# def read_segmentation_annotation_file(path, definitions):
+#     annotation = {}
+#     with open(path, 'r') as f:
+#         lines = f.readlines()
+#     for line in lines:
+#         items = line.strip().split('\t')
+#         label = items[0]
+#         if definitions.get(label) is None:
+#             print(f'{label} undefined label name ')
+#             exit(1)
+#         points = np.array([max(float(x), 0) for x in items[1:9]], dtype=np.int32).reshape(-1, 2)
+#         annotation[label] = points
+#     return annotation
+
+
+# def annotation_to_segmentation(image, annotation, definition):
+#     segmentation = np.zeros_like(image)
+#     # 그냥 fill 하면 background 가 Person Image 를 덮게 된다.
+#     # 꼼수이기는 하지만 annotation items 을 label 기준으로 sorting 해서
+#     # Background 가 항상 처음에 나오도록 한다.
+#     for label, points in sorted(annotation.items()):
+#         rgb = definition[label]
+#         cv2.fillPoly(segmentation, [points], tuple(rgb.tolist()), None)
+#     return segmentation
+
+
+# def make_mask_file_from_annotation(annotation_path, target_path, definition):
+#     for file_path in glob.glob(annotation_path + "*.jpg"):
+#         im = cv2.imread(file_path)
+#         annotation_file_path = file_path.replace(".jpg", ".txt")
+#         segmentation_annotation = read_segmentation_annotation_file(annotation_file_path, definition)
+#         segmentation_im = annotation_to_segmentation(im, segmentation_annotation, definition)
+#         cv2.imwrite(os.path.join(target_path, os.path.basename(file_path)), segmentation_im)
+
+
+# def create_image_patch_by_segmentation_annotation(annotation_path, target_path, definition):
+#     for file_path in glob.glob(annotation_path + "*.jpg"):
+#         im = cv2.imread(file_path)
+#         annotation_file_path = file_path.replace(".jpg", ".txt")
+#         segmentation_annotation = read_segmentation_annotation_file(annotation_file_path, definition)
+#         patch_images = extract_patch_images(im, segmentation_annotation)
+#         patch_name = os.path.join(target_path, os.path.basename(file_path).replace('.txt', ''))
+#         for label, image in patch_images.items():
+#             patch_path = patch_name + f'_{label}.png'
+#             cv2.imwrite(patch_path, image)
+
+
+# def create_image_patch_by_ocr_annotation(annotation_path, target_path, without_label=False):
+#     for (root, dirs, file_names) in os.walk(annotation_path):
+#         for file_name in file_names:
+#             file_path = os.path.join(root, file_name)
+#             if file_path.split('.')[-1] != 'jpg':
+#                 continue
+
+#             print(f'{file_path} is processing')
+#             im = cv2.imread(file_path)
+#             annotation_file_path = file_path.replace(".jpg", ".txt")
+#             if not os.path.exists(annotation_file_path):
+#                 print(f'error {annotation_file_path} did not exist')
+#                 continue
+#             ocr_annotation = read_ocr_annotation_file(annotation_file_path)            
+#             patch_images = extract_patch_images(im, ocr_annotation)
+#             patch_name = os.path.join(target_path, os.path.basename(file_path).replace('.jpg', ''))
+#             for index, (label, image) in enumerate(patch_images.items()):
+#                 if without_label:
+#                     patch_path = patch_name + f'_L__L_{index}.png'    
+#                 else:
+#                     patch_path = patch_name + f'_L_{label}_L_{index}.png'
+
+#                 try:
+#                     print(f'{patch_path}')
+#                     cv2.imwrite(patch_path, image)
+#                 except:
+#                     print(f'error {patch_path} has empty image for {file_path}')
+
+
+# def create_image_patch_by_ocr_annotation_file(file_path, target_path):
+#     im = cv2.imread(file_path)
+#     annotation_file_path = file_path.replace(".jpg", ".txt")
+#     if not os.path.exists(annotation_file_path):
+#         print(f'error {annotation_file_path} did not exist')
+#         return None
+
+#     ocr_annotation = read_ocr_annotation_file(annotation_file_path)
+#     patch_images = extract_patch_images(im, ocr_annotation)
+#     patch_name = os.path.join(target_path, os.path.basename(file_path).replace('.jpg', ''))
+#     for index, (label, image) in enumerate(patch_images.items()):
+#         patch_path = patch_name + f'_L_{label}_L_{index}.png'
+#         try:
+#             cv2.imwrite(patch_path, image)
+#         except:
+#             print(f'error {patch_path} has empty image for {file_path}')
+
+############ imageJ ocr ################
+def create_LMDB_by_ocr_annotation(annotation_path, target_path, divide_ratio={"train": 7, "val": 2, "test": 1}):
+    from lmdb_helper import MyLMDB
+    from random import sample, random
+    
+    lmdb_list = create_LMDBs(divide_ratio)
+    # *이나 #이 포함된 라벨만 따로 모아 놓음
+    lmdb_ignore = MyLMDB(f'{target_path}_ignore', sync_period=1000) 
+
+
+    for (root, dirs, file_names) in os.walk(annotation_path):
+        for file_name in file_names:
+            file_path = os.path.join(root, file_name)
+            extension = file_path.split('.')[-1]
+
+            if not is_img_file(extension):
+                continue
+
+            logging.info(f'{file_path} is processing')
+
+            im = read_img(file_path)
+            annotation_file_path = get_annotation_file_path(file_path, extension)
+            ocr_annotation = read_ocr_annotation_file(annotation_file_path)            
+            patch_images = extract_patch_images(im, ocr_annotation)
+
+            # 패치를 하나씩 읽어, 각 lmdb에 데이터 쓰기
+            for index, (label, im) in enumerate(patch_images.items()):                
+                if label.find("#") >= 0 or label.find("*") >= 0:
+                    lmdb_ignore.write_im_label_path(im, label, file_path, str(index))
+                    continue
+
+                h, w = im.shape[:2]
+                if h > w*1.5 and len(label) > 1:
+                    im = cv2.rotate(im, cv2.ROTATE_90_CLOCKWISE)
+                
+                ratios = [divide_ratio[x] for x in divide_ratio]
+                db_index = choose_lmdb(random(), ratios, len(divide_ratio))
+                lmdb_list[db_index].write_im_label_path(im, label, file_path, str(index))
+       
+    for lmdb in lmdb_list:
+        lmdb.close()
+    lmdb_ignore.close()
+
+
+def create_LMDBs(divide_ratio):
+    from lmdb_helper import MyLMDB
+
+    lmdb_list = []
+    for _, (db_type, ratio) in enumerate(divide_ratio.items()):
+        if not type(ratio) == int:
+            logging.error("divide_ratio 값이 int가 아닙니다.")
+            exit(1)
+        lmdb_list.append(MyLMDB(f'{target_path}_{db_type}', sync_period=1000))
+        logging.info(f"{db_type}이 비율 {ratio}로 생성됩니다.")
+    return lmdb_list
+
+
+def is_img_file(extension):
+    if extension not in ['jpg','jpeg']:
+        return False
+    return True
+
+
+def read_img(file_path):
+    im = cv2.imread(file_path)
+    if im is None:
+        logging.error(f'can not read image: {file_path}')
+        exit (-1)
+    return im
+
+
+def get_annotation_file_path(file_path, extension):
+    annotation_file_path = file_path.replace(extension, "txt")
+    if not os.path.exists(annotation_file_path):
+        logging.error(f'error {annotation_file_path} did not exist')
+        exit (-1)
+    return annotation_file_path
+
 
 def read_ocr_annotation_file(path):
     annotation = {}
@@ -28,63 +194,14 @@ def read_ocr_annotation_file(path):
     return annotation
 
 
-def read_segmentation_annotation_file(path, definitions):
-    annotation = {}
-    with open(path, 'r') as f:
-        lines = f.readlines()
-    for line in lines:
-        items = line.strip().split('\t')
-        label = items[0]
-        if definitions.get(label) is None:
-            print(f'{label} undefined label name ')
-            exit(1)
-        points = np.array([max(float(x), 0) for x in items[1:9]], dtype=np.int32).reshape(-1, 2)
-        annotation[label] = points
-    return annotation
-
-
-def annotation_to_segmentation(image, annotation, definition):
-    segmentation = np.zeros_like(image)
-    # 그냥 fill 하면 background 가 Person Image 를 덮게 된다.
-    # 꼼수이기는 하지만 annotation items 을 label 기준으로 sorting 해서
-    # Background 가 항상 처음에 나오도록 한다.
-    for label, points in sorted(annotation.items()):
-        rgb = definition[label]
-        cv2.fillPoly(segmentation, [points], tuple(rgb.tolist()), None)
-    return segmentation
-
-
-def make_mask_file_from_annotation(annotation_path, target_path, definition):
-    for file_path in glob.glob(annotation_path + "*.jpg"):
-        im = cv2.imread(file_path)
-        annotation_file_path = file_path.replace(".jpg", ".txt")
-        segmentation_annotation = read_segmentation_annotation_file(annotation_file_path, definition)
-        segmentation_im = annotation_to_segmentation(im, segmentation_annotation, definition)
-        cv2.imwrite(os.path.join(target_path, os.path.basename(file_path)), segmentation_im)
-
-
-def four_point_transform(image, rect):
-    (tl, tr, br, bl) = rect
-    if tl[0] < tr[0] and tl[1] < bl[1] and tr[1] < br[1] and bl[0] < br[0]:
-        pass
-    else:
-        print('error')
-    widthA = np.sqrt(((br[0] - bl[0]) ** 2) + ((br[1] - bl[1]) ** 2))
-    widthB = np.sqrt(((tr[0] - tl[0]) ** 2) + ((tr[1] - tl[1]) ** 2))
-    maxWidth = max(int(widthA), int(widthB))
-    # compute the height of the new image, which will be the
-    # maximum distance between the top-right and bottom-right
-    # y-coordinates or the top-left and bottom-left y-coordinates
-    heightA = np.sqrt(((tr[0] - br[0]) ** 2) + ((tr[1] - br[1]) ** 2))
-    heightB = np.sqrt(((tl[0] - bl[0]) ** 2) + ((tl[1] - bl[1]) ** 2))
-    maxHeight = max(int(heightA), int(heightB))
-    dst = np.array([[0, 0],[maxWidth - 1, 0],[maxWidth - 1, maxHeight - 1],[0, maxHeight - 1]], dtype = "float32")
-    # compute the perspective transform matrix and then apply it
-    M = cv2.getPerspectiveTransform(rect, dst)
-    warped = cv2.warpPerspective(image, M, (maxWidth, maxHeight))
-
-    cv2.imwrite('wapred.png', warped)
-    return warped
+def align_points(box):
+    # Make it clockwise align
+    # box = np.array([(int(float(x)), int(float(y))) for x, y in zip(items[1::2], items[2::2])])
+    centroid = np.sum(box, axis=0) / 4
+    theta = np.arctan2(box[:, 1] - centroid[1], box[:, 0] - centroid[0]) * 180 / np.pi
+    indices = np.argsort(theta)
+    aligned_box = box[indices]
+    return aligned_box
 
 
 def extract_patch_images(im, annotation, method='WARP_TRANSFORM'):
@@ -112,130 +229,48 @@ def extract_patch_images(im, annotation, method='WARP_TRANSFORM'):
     return patch_images
 
 
-def create_image_patch_by_segmentation_annotation(annotation_path, target_path, definition):
-    for file_path in glob.glob(annotation_path + "*.jpg"):
-        im = cv2.imread(file_path)
-        annotation_file_path = file_path.replace(".jpg", ".txt")
-        segmentation_annotation = read_segmentation_annotation_file(annotation_file_path, definition)
-        patch_images = extract_patch_images(im, segmentation_annotation)
-        patch_name = os.path.join(target_path, os.path.basename(file_path).replace('.txt', ''))
-        for label, image in patch_images.items():
-            patch_path = patch_name + f'_{label}.png'
-            cv2.imwrite(patch_path, image)
+def four_point_transform(image, rect):
+    (tl, tr, br, bl) = rect
+    if tl[0] < tr[0] and tl[1] < bl[1] and tr[1] < br[1] and bl[0] < br[0]:
+        pass
+    else:
+        print('error')
+    widthA = np.sqrt(((br[0] - bl[0]) ** 2) + ((br[1] - bl[1]) ** 2))
+    widthB = np.sqrt(((tr[0] - tl[0]) ** 2) + ((tr[1] - tl[1]) ** 2))
+    maxWidth = max(int(widthA), int(widthB))
+    # compute the height of the new image, which will be the
+    # maximum distance between the top-right and bottom-right
+    # y-coordinates or the top-left and bottom-left y-coordinates
+    heightA = np.sqrt(((tr[0] - br[0]) ** 2) + ((tr[1] - br[1]) ** 2))
+    heightB = np.sqrt(((tl[0] - bl[0]) ** 2) + ((tl[1] - bl[1]) ** 2))
+    maxHeight = max(int(heightA), int(heightB))
+    dst = np.array([[0, 0],[maxWidth - 1, 0],[maxWidth - 1, maxHeight - 1],[0, maxHeight - 1]], dtype = "float32")
+    # compute the perspective transform matrix and then apply it
+    M = cv2.getPerspectiveTransform(rect, dst)
+    warped = cv2.warpPerspective(image, M, (maxWidth, maxHeight))
+
+    cv2.imwrite('wapred.png', warped)
+    return warped
 
 
-def create_image_patch_by_ocr_annotation(annotation_path, target_path, without_label=False):
-    for (root, dirs, file_names) in os.walk(annotation_path):
-        for file_name in file_names:
-            file_path = os.path.join(root, file_name)
-            if file_path.split('.')[-1] != 'jpg':
-                continue
+def choose_lmdb(ran_num, ratios, num):
+    """
+    ran_num : 0~1 사이의 랜덤한 숫자
+    devide_ratio : 데이터를 나눌 비율, 이것으로 확률의 Treshold를 계산. ex) [7,2,1]
+    num : devide_ratio의 인덱스 갯수
+    return : devide_ratio 확률에 기반한, devide_ratio의 index
+    설명 : 재귀함수를 이용한, 리스트 안의 숫자의 크기를 참조하여, 인덱스를 리턴
+    """
+    if num < 0:
+        return
 
-            print(f'{file_path} is processing')
-            im = cv2.imread(file_path)
-            annotation_file_path = file_path.replace(".jpg", ".txt")
-            if not os.path.exists(annotation_file_path):
-                print(f'error {annotation_file_path} did not exist')
-                continue
-            ocr_annotation = read_ocr_annotation_file(annotation_file_path)            
-            patch_images = extract_patch_images(im, ocr_annotation)
-            patch_name = os.path.join(target_path, os.path.basename(file_path).replace('.jpg', ''))
-            for index, (label, image) in enumerate(patch_images.items()):
-                if without_label:
-                    patch_path = patch_name + f'_L__L_{index}.png'    
-                else:
-                    patch_path = patch_name + f'_L_{label}_L_{index}.png'
-
-                try:
-                    print(f'{patch_path}')
-                    cv2.imwrite(patch_path, image)
-                except:
-                    print(f'error {patch_path} has empty image for {file_path}')
-
-
-def create_LMDB_by_ocr_annotation(annotation_path, target_path):
-    from lmdb_helper import MyLMDB
-    from random import sample, random
-    # revise_path = "to_be_revised"
-    
-    # lmdb_revise = MyLMDB(revise_path, sync_period=1000)
-    # lmdb_test = MyLMDB(f'{target_path}_test', sync_period=1000)
-    lmdb_train = MyLMDB(f'{target_path}_train', sync_period=1000)
-    # lmdb_val = MyLMDB(f'{target_path}_val', sync_period=1000)
-
-    for (root, dirs, file_names) in os.walk(annotation_path):
-        for file_name in file_names:
-            file_path = os.path.join(root, file_name)
-            extension = file_path.split('.')[-1]
-            if extension == 'txt':
-                continue
-
-            print(f'{file_path} is processing')
-
-            if extension not in ['jpg', 'jpeg']:
-                continue 
-
-            im = cv2.imread(file_path)
-
-            if im is None:
-                print (f'can not read image: {file_path}')
-                exit (-1)
-
-            annotation_file_path = file_path.replace(extension, "txt")
-            if not os.path.exists(annotation_file_path):
-                print(f'error {annotation_file_path} did not exist')
-                continue
-            
-            ocr_annotation = read_ocr_annotation_file(annotation_file_path)            
-            patch_images = extract_patch_images(im, ocr_annotation)
-
-            # val_index = sample(range(0, 40000), k=8000)
-
-            count = 0 
-            for index, (label, im) in enumerate(patch_images.items()):                
-                h, w = im.shape[:2]
-                
-                # if label.find("#") >= 0 or label.find("*") >= 0 or h < 12:
-                # if label.find("#") >= 0 or label.find("*") >= 0:
-                #     lmdb_revise.write_im_label_path(im, label, file_path)
-                #     continue
-
-                if h > w*1.5 and len(label) > 1:
-                    im = cv2.rotate(im, cv2.ROTATE_90_CLOCKWISE)
-                
-                # if count in val_index:
-                #     lmdb_test.write_im_label_path(im, label, file_path)
-                # else:
-                #     if random() < 0.25:
-                #         lmdb_val.write_im_label_path(im, label, file_path)
-                #     else:
-                #         lmdb_train.write_im_label_path(im, label, file_path)
-
-                lmdb_train.write_im_label_path(im, label, file_path)
-                count += 1
-
-    lmdb_train.close() 
-    # lmdb_val.close() 
-    # lmdb_test.close() 
-    # lmdb_revise.close()
-
-            
-def create_image_patch_by_ocr_annotation_file(file_path, target_path):
-    im = cv2.imread(file_path)
-    annotation_file_path = file_path.replace(".jpg", ".txt")
-    if not os.path.exists(annotation_file_path):
-        print(f'error {annotation_file_path} did not exist')
-        return None
-
-    ocr_annotation = read_ocr_annotation_file(annotation_file_path)
-    patch_images = extract_patch_images(im, ocr_annotation)
-    patch_name = os.path.join(target_path, os.path.basename(file_path).replace('.jpg', ''))
-    for index, (label, image) in enumerate(patch_images.items()):
-        patch_path = patch_name + f'_L_{label}_L_{index}.png'
-        try:
-            cv2.imwrite(patch_path, image)
-        except:
-            print(f'error {patch_path} has empty image for {file_path}')
+    cur_index = len(ratios) - num
+    threshold = np.sum([ratios[x] for x in range(0, cur_index+1)])/np.sum(ratios)
+    if ran_num < threshold:
+        return cur_index
+    else:
+        num -= 1
+        return choose_lmdb(ran_num, ratios, num)
 
 
 if __name__ == '__main__':
@@ -265,7 +300,8 @@ if __name__ == '__main__':
     # annotation_file_path = file_path.replace('.jpg', '.txt')
     # ocr_annotation = read_ocr_annotation_file(annotation_file_path)            
     # patch_images = extract_patch_images(im, ocr_annotation)
+    
 
-    source_path = '/home/gucheol/다운로드/general_app_val'
-    target_path = '/home/gucheol/다운로드/general_app_val_lmdb'
-    create_LMDB_by_ocr_annotation(source_path, target_path)
+    source_path = '/home/gucheol/다운로드/test'
+    target_path = '/home/gucheol/다운로드/test_lmdb'
+    create_LMDB_by_ocr_annotation(source_path, target_path, divide_ratio={"train": 7, "val": 2, "test": 1})
